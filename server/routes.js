b9ed608f17a224d4beb7f997f29734c54af49acf
porrasmaldonadoanil339-spec <porrasmaldonadoanil339@gmail.com>
2026-03-27 10:56:43 -0500

    ocho locos
    the game ocho locos

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 import { createServer } from "node:http";
-import authRouter from "./auth";
-import { setupRooms } from "./rooms";
+import authRouter from "./auth.js";
+import { setupRooms } from "./rooms.js";
 export async function registerRoutes(app) {
     app.use("/api/auth", authRouter);
     const httpServer = createServer(app);
